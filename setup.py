15bfaf310e8822a3acc636c0edc0d2b46314f342
Advait-30 <advaitischels@gmail.com>
2025-03-10 15:59:48 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='LLM-Project',
+    version='0.0.0',
+    author='Advait',
+    author_email='advaitischels@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
